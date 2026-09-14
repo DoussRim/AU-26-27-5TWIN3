1e975531d1877c60d259af2d2d239038fdf486b5
DoussRim <rim.douss@esprit.tn>
2026-09-14 14:18:18 +0100

    S1

---
+++ Event/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from datetime import datetime
+from Person.models import Person
+# Create your models here.
+class Event(models.Model):
+    category_list=(
+        ('M','Musique'),
+        ('S','Sport'),
+        ('C','Cinema')
+    )
+    title=models.CharField("Titre",max_length=50)
+    description=models.TextField(max_length=50)
+    image =models.ImageField(upload_to="images/",null=True,blank=True)
+    category =models.CharField(choices=category_list,max_length=8)
+    state =models.BooleanField(default=False)
+    nbe_participant =models.IntegerField(default=0)
+    evt_date = models.DateTimeField()
+    creation_date=models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+    organizer=models.ForeignKey(
+        Person,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    participant=models.ManyToManyField(
+        Person,
+        through='Participants'
+    )
+    class Meta:
+        contraints=[
+            models.CheckConstraint(check=models.Q(
+                evt_date__gt=datetime.now()
+            ),name="Please Check event date")
+        ]
+class Participants(models.Model):
+    person=models.ForeignKey(Person,on_delete=models.CASCADE)
+    event=models.ForeignKey(Event,on_delete=models.CASCADE)
+    participation_date =models.DateTimeField(datetime.now(),auto_now_add=True)
+    class Meta:
+        unique_together=['person','event']
---
+++ Person/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+def VerifCin(value):
+    if len(value)!=8:
+        raise ValidationError('cin must have 8 characters')
+def VerifEmail(value):
+    if str(value).endswith('@esprit.tn'):
+        raise ValidationError(f'you email {value} must end with @esprit.tn')
+class Person(AbstractUser):
+    cin=models.CharField("CIN",max_length=8,primary_key=True,validators=[VerifCin])
+    email=models.EmailField(max_length=20,unique=True,validators=[VerifEmail])
+    username=models.CharField(max_length=20,unique=True)
+    
+    USERNAME_FIELD="username"
